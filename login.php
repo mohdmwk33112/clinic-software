e56cee1fba6e82a1cb219f7793d9df43e6962946
mohdmwk33112 <mohamed.wael18@msa.edu.eg>
2024-05-19 19:25:12 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,42 +1,11 @@
 <?php
-// Include config file
-require_once "config.php";
- include "classes.php";
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err= "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    // Validate name
-    $input_username = trim($_POST["username"]);
-    if(empty($input_username)){
-        $username_err = "Please enter a username.";
-    } elseif(!filter_var($input_username, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $username_err = "Please enter a valid name.";
-    } else{
-        $username = $input_username;
-    }
-    
-    // Validate address
-    $input_password = trim($_POST["password"]);
-    if(empty($input_password)){
-        $password_err = "Please enter a password.";     
-    } else{
-        $password = $input_password;
-    }
-    // Check input errors before inserting in database
-    if(empty($input_username) && empty($password_err) ){
-        $user=new user();
-        $result=$user->login($input_username,$input_password);
-        if(result==false){
-            
-        }
-        else{
-            
-        }
-    }
-}
+ require "classes.php";
+ require "config.php";
+ $error=" ";
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
+ $user=new user($_POST["username"],$_POST["password"],$link);
+ $error=$user->login();
+ }
 ?>
  
 <!DOCTYPE html>
@@ -44,7 +13,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Create Record</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <style type="text/css">
         .wrapper{
             width: 500px;
@@ -61,15 +30,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                         <h2>Log In</h2>
                     </div>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                        <div class="form-group">
                             <label>username</label>
-                            <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                            <span class="help-block"><?php echo $username_err;?></span>
+                            <input type="text" name="username" class="form-control">
                         </div>
-                        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                        <div class="form-group">
                             <label>password</label>
-                            <input type="password" name="password" class="form-control"><?php echo $password_err; ?></input>
-                            <span class="help-block"><?php echo $password_err;?></span>
+                            <input type="password" name="password" class="form-control"></input>
+                            <span class="help-block"><?php echo "$error";?></span>
                         </div>
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <a href="index.php" class="btn btn-default">Cancel</a>
