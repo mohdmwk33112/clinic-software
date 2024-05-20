f53c192791f82a6cdd3a02dee1febfcf721e24e8
mohdmwk33112 <mohamed.wael18@msa.edu.eg>
2024-05-20 16:17:25 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,50 +1,89 @@
-<?php
- require "classes.php";
- require "config.php";
- $error=" ";
- if($_SERVER["REQUEST_METHOD"] == "POST"){
- $user=new user($_POST["username"],$_POST["password"],$link);
- $error=$user->login();
- }
-?>
- 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Create Record</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <style type="text/css">
-        .wrapper{
-            width: 500px;
-            margin: 0 auto;
-        }
-    </style>
+    <?php include('includes/head.php'); ?>
 </head>
+
 <body>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h2>Log In</h2>
-                    </div>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="form-group">
-                            <label>username</label>
-                            <input type="text" name="username" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>password</label>
-                            <input type="password" name="password" class="form-control"></input>
-                            <span class="help-block"><?php echo "$error";?></span>
-                        </div>
-                        <input type="submit" class="btn btn-primary" value="Submit">
-                        <a href="index.php" class="btn btn-default">Cancel</a>
-                    </form>
+    <div class="wrapper wrapper-full-page">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute">
+            <div class="container">
+                <div class="navbar-wrapper">
+                    <a class="navbar-brand" href="#pablo">HopeView</a>
+                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                        aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-bar burger-lines"></span>
+                        <span class="navbar-toggler-bar burger-lines"></span>
+                        <span class="navbar-toggler-bar burger-lines"></span>
+                    </button>
                 </div>
-            </div>        
+                <div class="collapse navbar-collapse justify-content-end" id="navbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a href="login.php" class="nav-link">
+                                <i class="nc-icon nc-mobile"></i> Login
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- End Navbar -->
+        <div class="full-page section-image" data-color="black" data-image="assets/img/full-screen-image-2.jpg">
+            <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
+            <div class="content">
+                <div class="container">
+                    <div class="col-md-4 col-sm-6 ml-auto mr-auto">
+                        <form class="form" method="post" action="login.php">
+                            <div class="card card-login card-hidden">
+                                <div class="card-header">
+                                    <h3 class="header text-center">Login</h3>
+                                </div>
+                                <div class="card-body">
+                                    <?php
+                                    require "classes.php";
+                                    require "config.php";
+                                    if (isset($_POST["login"])) {
+                                        $username = $_POST["username"];
+                                        $password = $_POST["password"];
+                                        $user1 = new user($username, $password, $link);
+                                        echo $user1->login();
+                                    }
+                                    ?>
+                                    <div class="form-group">
+                                        <label>Username</label>
+                                        <input type="text" placeholder="Enter Username" class="form-control" name="username" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Password</label>
+                                        <input type="password" placeholder="Password" class="form-control" name="password" required>
+                                    </div>
+                                </div>
+                                <div class="card-footer ml-auto mr-auto">
+                                    <button type="submit" class="btn btn-primary btn-wd" name="login">Login</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
+        <?php include('includes/footer.php'); ?>
     </div>
 </body>
+
+<?php include('includes/scripts.php'); ?>
+<script>
+    $(document).ready(function () {
+        demo.checkFullPageBackgroundImage();
+
+        setTimeout(function () {
+            // after 1000 ms we add the class animated to the login/register card
+            $('.card').removeClass('card-hidden');
+        }, 700);
+    });
+</script>
+
 </html>
